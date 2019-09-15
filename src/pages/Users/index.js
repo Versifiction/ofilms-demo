39400b7f1878055f1f6b67af9022aee50c6f7772
Versifiction <marc_charpentier@hotmail.fr>
2019-09-15 22:57:59 +0200

    suppression page users bdd

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import useForceUpdate from "use-force-update";
+import moment from "moment";
 
 import "../../App.css";
 import Spinner from "../../components/Molecules/Spinner";
@@ -27,35 +29,48 @@ function Users() {
       console.log(error);
     }
   }
+
+  function deleteUser(id) {
+    axios
+      .get(`http://localhost:4000/user/delete/${id}`)
+      .then(console.log("Deleted"))
+      .catch(err => console.log(err));
+  }
+
   return (
     <>
       <Nav />
-      <div className="container">
-        <h2>Utilisateurs</h2>
-        {pending ? (
-          <Spinner />
-        ) : (
-          <>
-            <table
-              className="striped responsive-table"
-              style={{ color: "white" }}
-            >
-              <thead style={{ borderBottom: "1px solid white" }}>
-                <tr>
-                  <th>ID</th>
-                  <th>E-mail</th>
-                  <th>Prénom</th>
-                  <th>Nom</th>
-                  <th>Pseudo</th>
-                  <th>Sexe</th>
-                  <th>Téléphone mobile</th>
-                  <th>Ville</th>
-                  <th>Code postal</th>
-                </tr>
-              </thead>
-              <tbody>
-                {usersList &&
-                  usersList.map(user => (
+      <h2>Utilisateurs</h2>
+      {pending ? (
+        <Spinner />
+      ) : (
+        <>
+          <table
+            className="striped responsive-table"
+            style={{ color: "white" }}
+          >
+            <thead style={{ borderBottom: "1px solid white" }}>
+              <tr>
+                <th>ID</th>
+                <th>E-mail</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Pseudo</th>
+                <th>Sexe</th>
+                <th>Téléphone mobile</th>
+                <th>Ville</th>
+                <th>Code postal</th>
+                <th>Admin</th>
+                <th>Modérateur</th>
+                <th>Connecté</th>
+                <th>Date d'inscription</th>
+                <th>Dernière connexion</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersList &&
+                usersList.map(user => (
+                  <>
                     <tr
                       key={user._id}
                       style={{ backgroundColor: "transparent" }}
@@ -69,13 +84,100 @@ function Users() {
                       <td>{user.mobilePhone}</td>
                       <td>{user.city}</td>
                       <td>{user.postalCode}</td>
+                      <td>
+                        {user.isAdmin ? (
+                          <i class="material-icons" style={{ color: "green" }}>
+                            check
+                          </i>
+                        ) : (
+                          <i class="material-icons" style={{ color: "red" }}>
+                            close
+                          </i>
+                        )}
+                      </td>
+                      <td>
+                        {user.isModerator ? (
+                          <i class="material-icons" style={{ color: "green" }}>
+                            check
+                          </i>
+                        ) : (
+                          <i class="material-icons" style={{ color: "red" }}>
+                            close
+                          </i>
+                        )}
+                      </td>
+                      <td>
+                        {user.isConnected ? (
+                          <i class="material-icons" style={{ color: "green" }}>
+                            check
+                          </i>
+                        ) : (
+                          <i class="material-icons" style={{ color: "red" }}>
+                            close
+                          </i>
+                        )}
+                      </td>
+                      <td>
+                        {moment(user.creationDate).format(
+                          "DD/MM/YYYY à hh:mm:ss"
+                        )}
+                      </td>
+                      <td>
+                        {moment(user.lastConnection).format(
+                          "DD/MM/YYYY à hh:mm:ss"
+                        )}
+                      </td>
+                      <td>
+                        <div
+                          className="waves-effect waves-light btn modal-trigger"
+                          href="#modal1"
+                          style={{
+                            backgroundColor: "red",
+                            marginTop: "inherit"
+                          }}
+                        >
+                          Supprimer
+                        </div>
+                      </td>
                     </tr>
-                  ))}
-              </tbody>
-            </table>
-          </>
-        )}
-      </div>
+                    <div id="modal1" class="modal">
+                      <div class="modal-content">
+                        <h4 style={{ color: "black" }}>
+                          Suppression d'utilisateur
+                        </h4>
+                        <p>
+                          Vous êtes sur le point de supprimer l'utilisateur.
+                          Voulez-vous le supprimer ?
+                        </p>
+                      </div>
+                      <div class="modal-footer" style={{ textAlign: "center" }}>
+                        <div
+                          class="modal-close waves-effect waves-light btn"
+                          style={{ backgroundColor: "green" }}
+                          onClick={() => {
+                            deleteUser(user._id);
+                          }}
+                        >
+                          Oui
+                        </div>
+                        <div
+                          class="modal-close waves-effect waves-green btn-flat"
+                          style={{
+                            backgroundColor: "red",
+                            color: "white",
+                            marginLeft: "10px"
+                          }}
+                        >
+                          Non
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   );
 }
